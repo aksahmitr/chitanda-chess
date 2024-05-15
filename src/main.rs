38eebdd569642cb54652ce8f002f43b1de47fe46
Akash Mitra <am.akashmitra1000@gmail.com>
2024-05-15 22:23:09 +0530

    upper lower mask fix

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod board;
 mod lookup;
 
 fn main() {
-    let cur =
-        board::Board::from_fen("r1b1kbnr/pp1ppppp/2p5/1q6/3nP3/5N1P/PPP1BPP1/RNBQK2R w KQkq - 1 6")
-            .unwrap();
+    let cur = board::Board::from_fen(
+        "r1b1kbnr/pp1p1ppp/q1p5/2R1p3/P3P3/5N1P/1Pn1BPP1/1NBQ1K1R w kq e6 0 11",
+    )
+    .unwrap();
 
     println!("{:#?}", cur.get_moves(board::PlayerColor::White));
 }
